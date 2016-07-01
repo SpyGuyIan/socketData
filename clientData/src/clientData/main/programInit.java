184e124cf1adcde4e172ddc94cf7f3c4d66fa15b
SpyGuyIan <spyguyian@gmail.com>
2016-07-01 14:11:30 -0400

    moved server and client and fixed errors from that

--- clientData/src/clientData/main/programInit.java
+++ clientData/src/clientData/main/programInit.java
@@ -7,6 +7,7 @@ public class programInit {
 	public static void main(String[] args) {
 		Game ex = new Game();
 		ex.setVisible(true);
+		new mainClass();
 	}
 
 }
--- serverData/src/serverData/main/programInit.java
+++ serverData/src/serverData/main/programInit.java
@@ -1,12 +1,9 @@
 package serverData.main;
 
-import serverData.GUI.Game;
-
 public class programInit {
 	
 	public static void main(String[] args) {
-		Game ex = new Game();
-		ex.setVisible(true);
+		new mainClass();
 	}
 
 }
